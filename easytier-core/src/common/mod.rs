5cbc2f681f6207c91bf652179892e9c473237ac1
sijie.sun <sijie.sun@smartx.com>
2024-02-27 21:39:06 +0800

    fix local time not work in musl

--- easytier-core/src/common/mod.rs
+++ easytier-core/src/common/mod.rs
@@ -7,3 +7,19 @@ pub mod netns;
 pub mod network;
 pub mod rkyv_util;
 pub mod stun;
+
+pub fn get_logger_timer<F: time::formatting::Formattable>(
+    format: F,
+) -> tracing_subscriber::fmt::time::OffsetTime<F> {
+    unsafe {
+        time::util::local_offset::set_soundness(time::util::local_offset::Soundness::Unsound)
+    };
+    let local_offset = time::UtcOffset::current_local_offset()
+        .unwrap_or(time::UtcOffset::from_whole_seconds(0).unwrap());
+    tracing_subscriber::fmt::time::OffsetTime::new(local_offset, format)
+}
+
+pub fn get_logger_timer_rfc3339(
+) -> tracing_subscriber::fmt::time::OffsetTime<time::format_description::well_known::Rfc3339> {
+    get_logger_timer(time::format_description::well_known::Rfc3339)
+}
