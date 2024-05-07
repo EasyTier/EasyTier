189a073f05a268eb040eaeeda6d9c499b2e8c2ab
m1m1sha <18262227804@163.com>
2024-05-07 14:30:40 +0800

    🌈 style: lint

--- easytier-gui/src/vite-env.d.ts
+++ easytier-gui/src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+
+  const component: DefineComponent<object, object, any>
+  export default component
 }
