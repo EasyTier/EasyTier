3b1653fba63e44177145a4f8c109e6ba498359f6
sijie.sun <sijie.sun@smartx.com>
2024-09-18 22:12:21 +0800

    fix connect failed after setup one of sockets fails

--- easytier/src/tunnel/tcp.rs
+++ easytier/src/tunnel/tcp.rs
@@ -139,7 +139,11 @@ impl TcpTunnelConnector {
                 socket2::Type::STREAM,
                 Some(socket2::Protocol::TCP),
             )?;
-            setup_sokcet2(&socket2_socket, bind_addr)?;
+
+            if let Err(e) = setup_sokcet2(&socket2_socket, bind_addr) {
+                tracing::error!(bind_addr = ?bind_addr, ?addr, "bind addr fail: {:?}", e);
+                continue;
+            }
 
             let socket = TcpSocket::from_std_stream(socket2_socket.into());
             futures.push(socket.connect(addr.clone()));
