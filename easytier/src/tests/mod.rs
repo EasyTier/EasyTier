4d6534bff985f36d665b9e3df4748658694378f5
fanyang89 <fanyang89@outlook.com>
2024-10-23 20:31:47 +0800

    Fix compile

--- easytier/src/tests/mod.rs
+++ easytier/src/tests/mod.rs
@@ -1,8 +1,8 @@
 #[cfg(target_os = "linux")]
 mod three_node;
 
-use easytier::common::PeerId;
-use easytier::peers;
+use crate::common::PeerId;
+use crate::peers::peer_manager::PeerManager;
 
 pub fn get_guest_veth_name(net_ns: &str) -> &str {
     Box::leak(format!("veth_{}_g", net_ns).into_boxed_str())
@@ -144,7 +144,7 @@ fn check_route(ipv4: &str, dst_peer_id: PeerId, routes: Vec<crate::proto::cli::R
 }
 
 async fn wait_proxy_route_appear(
-    mgr: &std::sync::Arc<peers::peer_manager::PeerManager>,
+    mgr: &std::sync::Arc<PeerManager>,
     ipv4: &str,
     dst_peer_id: PeerId,
     proxy_cidr: &str,
