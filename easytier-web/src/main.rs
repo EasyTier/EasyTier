a3f3ae24f89e53828a93d85ff7953a8cf8133c2e
sijie.sun <sijie.sun@smartx.com>
2024-10-28 09:37:22 +0800

    improve log

--- easytier-web/src/main.rs
+++ easytier-web/src/main.rs
@@ -2,13 +2,23 @@
 
 use std::sync::Arc;
 
-use easytier::tunnel::udp::UdpTunnelListener;
+use easytier::{
+    common::config::{ConfigLoader, ConsoleLoggerConfig, TomlConfigLoader},
+    tunnel::udp::UdpTunnelListener,
+    utils::init_logger,
+};
 
 mod client_manager;
 mod restful;
 
 #[tokio::main]
 async fn main() {
+    let config = TomlConfigLoader::default();
+    config.set_console_logger_config(ConsoleLoggerConfig {
+        level: Some("debug".to_string()),
+    });
+    init_logger(config, false).unwrap();
+
     let listener = UdpTunnelListener::new("udp://0.0.0.0:22020".parse().unwrap());
     let mut mgr = client_manager::ClientManager::new();
     mgr.serve(listener).await.unwrap();
