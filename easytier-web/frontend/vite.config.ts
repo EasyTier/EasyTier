cc6a0c94255e91754156eac37a04c7fe7fb5d7df
sijie.sun <sijie.sun@smartx.com>
2024-11-08 22:52:47 +0800

    single file

--- easytier-web/frontend/vite.config.ts
+++ easytier-web/frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteSingleFile()],
 })
