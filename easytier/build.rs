bc7c4d8cd0fabc9507ac4ed301c1da8d4245858c
Sophon <strongtz@yeah.net>
2024-11-21 10:32:03 +0800

    Fix building on Windows ARM64 (#485)
    Tested building natively on Windows ARM64, with Rust 1.82 and VS2022 toolchains.
    Basic functionality of easytier-cli, easytier-core and easytier-web has been verified.

--- easytier/build.rs
+++ easytier/build.rs
@@ -67,7 +67,12 @@ impl WindowsBuild {
 
     pub fn check_for_win() {
         // add third_party dir to link search path
+        #[cfg(target_arch = "x86_64")]
         println!("cargo:rustc-link-search=native=easytier/third_party/");
+
+        #[cfg(target_arch = "aarch64")]
+        println!("cargo:rustc-link-search=native=easytier/third_party/arm64/");
+
         let protoc_path = if let Some(o) = Self::check_protoc_exist() {
             println!("cargo:info=use os exist protoc: {:?}", o);
             o
