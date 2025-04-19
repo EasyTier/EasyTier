6d724c7d3629a251cadd02a0a80cb69675107d5a
unknown <764475881@qq.com>
2025-04-19 19:09:57 +0800

    新增magisk支持

--- easytier-contrib/easytier-magisk/build.sh
+++ easytier-contrib/easytier-magisk/build.sh
@@ -4,20 +4,22 @@ version=$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}' | sed 's/ 
 
 version='v'$(grep '^version =' ../../easytier/Cargo.toml | cut -d '"' -f 2)
 
-filename="easytier_magisk_${version}.zip"
-echo $version  # 输出: 2.2.4
-
-
-if [ -d "./artifacts/" ]; then
-    cp ./artifacts/easytier-core ./
-    cp ./artifacts/easytier-cli ./
-    cp ./artifacts/easytier-web ./
-else
-    wget -O "easytier_last.zip" https://github.com/EasyTier/EasyTier/releases/download/"$version"/easytier-linux-aarch64-"$version".zip
-    unzip -o -d ./ easytier_last.zip
-    rm -rf ./easytier_last.zip
-    rm -rf ./easytier-linux-aarch64
+if [ -z "$version" ]; then
+    echo "Error: 版本号不存在."
+    exit 1
 fi
 
+filename="easytier_magisk_${version}.zip"
+echo $version  
 
-zip -r -o -X -ll "$filename" ./ -x '.git/*' -x '.github/*' -x 'folder/*' -x 'build.sh' -x 'magisk_update.json'
+
+if [ -f "./easytier-core" ] && [ -f "./easytier-cli" ] && [ -f "./easytier-web" ]; then
+    zip -r -o -X "$filename" ./ -x '.git/*' -x '.github/*' -x 'folder/*' -x 'build.sh' -x 'magisk_update.json'
+else
+    wget -O "easytier_last.zip" https://github.com/EasyTier/EasyTier/releases/download/"$version"/easytier-linux-aarch64-"$version".zip
+    unzip -o easytier_last.zip -d ./
+    mv ./easytier-linux-aarch64/* ./
+    rm -rf ./easytier_last.zip
+    rm -rf ./easytier-linux-aarch64
+    zip -r -o -X "$filename" ./ -x '.git/*' -x '.github/*' -x 'folder/*' -x 'build.sh' -x 'magisk_update.json'
+fi
