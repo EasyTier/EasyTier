b5f60f843d1986ce4f454f74d5b46ba15ec1dd6d
Sijie.Sun <sunsijie@buaa.edu.cn>
2025-04-19 22:56:20 +0800

    set web assets base dir from env (#793)

--- easytier-web/frontend/vite.config.ts
+++ easytier-web/frontend/vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import { viteSingleFile } from "vite-plugin-singlefile"
 
+const WEB_BASE_URL = process.env.WEB_BASE_URL || '';
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '',
+  base: WEB_BASE_URL,
   plugins: [vue(),/* viteSingleFile() */],
 })
