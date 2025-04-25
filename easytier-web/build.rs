6be04ae3968d2d5bed4da656db034b98b18dcec2
loecomm <loecom@qq.com>
2025-04-25 16:50:51 +0800

    web add thunk-rs

---
+++ easytier-web/build.rs
@@ -0,0 +1,7 @@
+fn main() {
+	// enable thunk-rs when target os is windows and arch is i686
+	#[cfg(target_os = "windows")]
+    if !std::env::var("TARGET").unwrap_or_default().contains("aarch64"){
+        thunk::thunk();
+    }
+}
