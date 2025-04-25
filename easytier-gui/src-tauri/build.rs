3998499b8b69354f9ca34f029bb8767a10b188a9
loecomm <loecom@qq.com>
2025-04-25 09:47:40 +0800

    add YY_Thunks to support win7

--- easytier-gui/src-tauri/build.rs
+++ easytier-gui/src-tauri/build.rs
@@ -1,9 +1,3 @@
 fn main() {
-	// enable thunk-rs when target os is windows and arch is i686
-	#[cfg(target_os = "windows")]
-    if !std::env::var("TARGET").unwrap_or_default().contains("aarch64"){
-        thunk::thunk();
-    }
-
     tauri_build::build();
 }
--- easytier/build.rs
+++ easytier/build.rs
@@ -127,12 +127,6 @@ fn check_locale() {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	// enable thunk-rs when target os is windows and arch is i686
-	#[cfg(target_os = "windows")]
-    if !std::env::var("TARGET").unwrap_or_default().contains("aarch64"){
-        thunk::thunk();
-    }
-
     #[cfg(target_os = "windows")]
     WindowsBuild::check_for_win();
 
