700a0c3a7270c88261cdaf5ee05a8b4896ff1fc8
chenxudong2020 <872603935@qq.com>
2025-06-04 21:21:58 +0800

    Update websocket.rs

--- easytier/src/tunnel/websocket.rs
+++ easytier/src/tunnel/websocket.rs
@@ -204,7 +204,7 @@ impl WSTunnelConnector {
             init_crypto_provider();
             let tls_conn =
                 tokio_rustls::TlsConnector::from(Arc::new(get_insecure_tls_client_config()));
-            // Modify SNI logic: always use "localhost" as SNI to avoid IP blocking
+            // Modify SNI logic: always use "localhost" as SNI to avoid IP blocking.
             let sni = "localhost";
             let server_name = rustls::ServerName::try_from(sni)
                 .map_err(|_| TunnelError::InvalidProtocol("Invalid SNI".to_string()))?;
