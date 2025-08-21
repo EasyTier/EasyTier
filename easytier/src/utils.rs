df7eb4759368ffa86b9f844f53bd826ad2897c3d
fanyang <fanyang89@outlook.com>
2025-08-21 11:41:42 +0800

    Support tokio-console (#1259)

--- easytier/src/utils.rs
+++ easytier/src/utils.rs
@@ -2,7 +2,9 @@ use std::{fs::OpenOptions, str::FromStr};
 
 use anyhow::Context;
 use tracing::level_filters::LevelFilter;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
+use tracing_subscriber::{
+    layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer, Registry,
+};
 
 use crate::common::{config::LoggingConfigLoader, get_logger_timer_rfc3339};
 
@@ -102,10 +104,22 @@ pub fn init_logger(
         .with_writer(std::io::stderr)
         .with_filter(console_filter);
 
-    tracing_subscriber::Registry::default()
-        .with(console_layer)
-        .with(file_layer)
-        .init();
+    let registry = Registry::default();
+
+    #[cfg(not(feature = "tracing"))]
+    {
+        registry.with(console_layer).with(file_layer).init();
+    }
+
+    #[cfg(feature = "tracing")]
+    {
+        let console_subscriber_layer = console_subscriber::ConsoleLayer::builder().spawn();
+        registry
+            .with(console_layer)
+            .with(file_layer)
+            .with(console_subscriber_layer)
+            .init();
+    }
 
     Ok(ret_sender)
 }
