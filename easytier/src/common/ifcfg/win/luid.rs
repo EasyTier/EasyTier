bd8865219ff137b13c583fea0ba83b5eb6d34e1b
sijie.sun <sijie.sun@smartx.com>
2025-08-26 22:10:32 +0800

    fix set ipv6 mtu may cause tun init error

--- easytier/src/common/ifcfg/win/luid.rs
+++ easytier/src/common/ifcfg/win/luid.rs
@@ -701,8 +701,12 @@ impl InterfaceLuid {
         // SAFETY: Both NET_LUID_LH unions should be the same. We're just copying out
         // the u64 value and re-wrapping it, since wintun doesn't refer to the windows
         // crate's version of NET_LUID_LH.
-        self.try_set_mtu(AF_INET as ADDRESS_FAMILY, mtu)?;
-        self.try_set_mtu(AF_INET6 as ADDRESS_FAMILY, mtu)?;
+        if let Err(e) = self.try_set_mtu(AF_INET as ADDRESS_FAMILY, mtu) {
+            tracing::warn!("Failed to set IPv4 MTU: {:?}", e);
+        }
+        if let Err(e) = self.try_set_mtu(AF_INET6 as ADDRESS_FAMILY, mtu) {
+            tracing::warn!("Failed to set IPv6 MTU: {:?}", e);
+        }
         Ok(())
     }
 
