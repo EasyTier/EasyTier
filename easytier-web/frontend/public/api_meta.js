ad6f6922f606d68e791c63d7033c5ada787a33d4
a2942_neko <77714719+a2942@users.noreply.github.com>
2025-12-11 11:38:42 +0800

    Use location.origin for api_host in api_meta.js
    修改 api_host 默认值为打开的域名而非固定域名

--- easytier-web/frontend/public/api_meta.js
+++ easytier-web/frontend/public/api_meta.js
@@ -1,3 +1,7 @@
 window.apiMeta = {
-    api_host: "https://config-server.easytier.cn"
+    api_host: location.origin //使用 location.origin 代替原本固定域名
+    // 备用选项，使用默认的80端口建立面板导致连接失败时取消注释以下内容
+    // const defaultPort = location.protocol === 'https:' ? '443' : '80';
+    // const baseUrl = location.protocol + '//' + location.hostname + ':' + (location.port || defaultPort);
+    // api_host: baseUrl
 }
