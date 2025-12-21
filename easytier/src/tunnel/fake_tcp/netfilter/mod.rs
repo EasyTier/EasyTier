b475d5cf84598663611e159b5a2a9b22370da7e8
sijie.sun <sijie.sun@smartx.com>
2025-12-21 20:56:58 +0800

    add macos

--- easytier/src/tunnel/fake_tcp/netfilter/mod.rs
+++ easytier/src/tunnel/fake_tcp/netfilter/mod.rs
@@ -6,6 +6,9 @@ use crate::tunnel::fake_tcp::stack;
 #[cfg(target_os = "linux")]
 pub mod linux_bpf;
 
+#[cfg(target_os = "macos")]
+pub mod macos_bpf;
+
 #[cfg(target_os = "windows")]
 pub mod windivert;
 
@@ -33,7 +36,29 @@ pub fn create_tun(
     }
 }
 
-#[cfg(all(not(windows), not(target_os = "linux")))]
+#[cfg(target_os = "macos")]
+pub fn create_tun(
+    interface_name: &str,
+    src_addr: Option<SocketAddr>,
+    dst_addr: SocketAddr,
+) -> Arc<dyn super::stack::Tun> {
+    match macos_bpf::MacosBpfTun::new(interface_name, src_addr, dst_addr) {
+        Ok(tun) => Arc::new(tun),
+        Err(e) => {
+            tracing::warn!(
+                ?e,
+                interface_name,
+                "MacosBpfTun init failed, falling back to PnetTun"
+            );
+            Arc::new(pnet::PnetTun::new(
+                interface_name,
+                pnet::create_packet_filter(src_addr, dst_addr),
+            ))
+        }
+    }
+}
+
+#[cfg(all(not(windows), not(target_os = "linux"), not(target_os = "macos")))]
 pub fn create_tun(
     interface_name: &str,
     src_addr: Option<SocketAddr>,
