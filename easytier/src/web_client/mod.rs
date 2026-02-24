9547ca785aa0e31ff2639b64a5cbfe612e756b90
hello <9403759+cfdworld@users.noreply.github.com>
2026-02-24 12:53:24 +0800

    core wss use last segments as token

--- easytier/src/web_client/mod.rs
+++ easytier/src/web_client/mod.rs
@@ -133,7 +133,7 @@ pub async fn run_web_client(
     //c_url.set_path("");
     let token = config_server_url
         .path_segments()
-        .and_then(|mut x| x.next())
+        .and_then(|x| x.last())
         .map(|x| percent_encoding::percent_decode_str(x).decode_utf8())
         .transpose()
         .with_context(|| "failed to decode config server token")?
