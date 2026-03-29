3fe3ff004af90e9d8e1bfa38d199813e59540039
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2026-03-30 00:30:59 +0200

    add BoxExt trait

--- easytier/src/utils.rs
+++ easytier/src/utils.rs
@@ -141,3 +141,11 @@ pub fn weak_upgrade<T>(weak: &std::sync::Weak<T>) -> anyhow::Result<std::sync::A
     weak.upgrade()
         .ok_or_else(|| anyhow::anyhow!("{} not available", std::any::type_name::<T>()))
 }
+
+pub trait BoxExt: Sized {
+    fn boxed(self) -> Box<Self> {
+        Box::new(self)
+    }
+}
+
+impl<T> BoxExt for T {}
