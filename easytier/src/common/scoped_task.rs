10aaab638a426600b50fb269ee0c9acc2e360631
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2026-04-07 00:43:09 +0200

    simplify ScopedTask
    scoped_task
    
    Revert "scoped_task"
    
    This reverts commit 4a3e9f317fc22eb29e9aadc56f3b260b89a9b95f.

--- easytier/src/common/scoped_task.rs
+++ easytier/src/common/scoped_task.rs
@@ -4,40 +4,25 @@
 //! For example, if task A spawned task B but is doing something else, and task B is waiting for task C to join,
 //! aborting A will also abort both B and C.
 
+use derive_more::{Deref, DerefMut, From};
 use std::future::Future;
-use std::ops::Deref;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 use tokio::task::JoinHandle;
 
-#[derive(Debug)]
-pub struct ScopedTask<T> {
-    inner: JoinHandle<T>,
-}
+#[derive(Debug, From, Deref, DerefMut)]
+pub struct ScopedTask<T>(JoinHandle<T>);
 
 impl<T> Drop for ScopedTask<T> {
     fn drop(&mut self) {
-        self.inner.abort()
+        self.abort()
     }
 }
 
 impl<T> Future for ScopedTask<T> {
     type Output = <JoinHandle<T> as Future>::Output;
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        Pin::new(&mut self.inner).poll(cx)
-    }
-}
-
-impl<T> From<JoinHandle<T>> for ScopedTask<T> {
-    fn from(inner: JoinHandle<T>) -> Self {
-        Self { inner }
-    }
-}
-
-impl<T> Deref for ScopedTask<T> {
-    type Target = JoinHandle<T>;
-    fn deref(&self) -> &Self::Target {
-        &self.inner
+        Pin::new(&mut self.0).poll(cx)
     }
 }
 
