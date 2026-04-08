8a29a94e13e390e222bf3fdb8a9ac017a68afe80
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2026-04-09 00:44:33 +0200

    add thunk-rs to build-deps

--- easytier-gui/src-tauri/build.rs
+++ easytier-gui/src-tauri/build.rs
@@ -1,10 +1,10 @@
+use std::env;
+
 fn main() {
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
     // enable thunk-rs when target os is windows and arch is x86_64 or i686
-    #[cfg(target_os = "windows")]
-    if !std::env::var("TARGET")
-        .unwrap_or_default()
-        .contains("aarch64")
-    {
+    if target_os == "windows" && (target_arch == "x86" || target_arch == "x86_64") {
         thunk::thunk();
     }
 
