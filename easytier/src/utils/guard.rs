7e9d71b766965985ffd76dba844da7967dc1784b
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2026-04-19 19:27:14 +0200

    utils: add DetachableTask & ContextGuard
    add guard macros
    
    guard
    
    unit tests
    
    clippy
    
    panic message
    
    task

---
+++ easytier/src/utils/guard.rs
@@ -0,0 +1,254 @@
+use crate::utils::task::{DetachableTask, TaskSpawner};
+use std::mem::ManuallyDrop;
+use std::ops::{Deref, DerefMut};
+
+pub trait CallableGuard<const ASYNC: bool, Context> {
+    type Output;
+    fn call(self, context: Context) -> Self::Output;
+}
+
+impl<Context, Guard> CallableGuard<false, Context> for Guard
+where
+    Guard: FnOnce(Context),
+{
+    type Output = ();
+
+    fn call(self, context: Context) -> Self::Output {
+        self(context)
+    }
+}
+
+impl<Context, Guard, Task> CallableGuard<true, Context> for Guard
+where
+    Guard: FnOnce(Context) -> Task + Send + 'static,
+    Task: Future<Output = ()> + Send + 'static,
+{
+    type Output = DetachableTask<TaskSpawner<Task>, Task>;
+
+    fn call(self, context: Context) -> Self::Output {
+        DetachableTask::new(self(context))
+    }
+}
+
+pub struct ContextGuard<const ASYNC: bool, Context, Guard: CallableGuard<ASYNC, Context>> {
+    context: ManuallyDrop<Context>,
+    guard: ManuallyDrop<Guard>,
+}
+
+impl<const ASYNC: bool, Context, Guard: CallableGuard<ASYNC, Context>> Deref
+    for ContextGuard<ASYNC, Context, Guard>
+{
+    type Target = Context;
+
+    fn deref(&self) -> &Self::Target {
+        &self.context
+    }
+}
+
+impl<const ASYNC: bool, Context, Guard: CallableGuard<ASYNC, Context>> DerefMut
+    for ContextGuard<ASYNC, Context, Guard>
+{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.context
+    }
+}
+
+impl<const ASYNC: bool, Context, Guard: CallableGuard<ASYNC, Context>>
+    ContextGuard<ASYNC, Context, Guard>
+{
+    pub fn new<_R>(context: Context, guard: Guard) -> Self
+    where
+        Guard: FnOnce(Context) -> _R,
+    {
+        ContextGuard {
+            context: ManuallyDrop::new(context),
+            guard: ManuallyDrop::new(guard),
+        }
+    }
+}
+
+impl<const ASYNC: bool, Context, Guard: CallableGuard<ASYNC, Context>>
+    ContextGuard<ASYNC, Context, Guard>
+{
+    unsafe fn call(&mut self) -> Guard::Output {
+        unsafe {
+            let context = ManuallyDrop::take(&mut self.context);
+            let guard = ManuallyDrop::take(&mut self.guard);
+
+            guard.call(context)
+        }
+    }
+
+    pub fn trigger(self) -> Guard::Output {
+        let mut this = ManuallyDrop::new(self);
+        unsafe { this.call() }
+    }
+
+    pub fn defuse(self) -> Context {
+        let mut this = ManuallyDrop::new(self);
+        unsafe {
+            ManuallyDrop::drop(&mut this.guard);
+            ManuallyDrop::take(&mut this.context)
+        }
+    }
+}
+
+impl<const ASYNC: bool, Context, Guard: CallableGuard<ASYNC, Context>> Drop
+    for ContextGuard<ASYNC, Context, Guard>
+{
+    fn drop(&mut self) {
+        if let Err(error) = std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| {
+            let _ = unsafe { self.call() };
+        })) {
+            let msg = error
+                .downcast_ref::<String>()
+                .map(String::as_str)
+                .or_else(|| error.downcast_ref::<&str>().copied())
+                .unwrap_or("<unknown panic payload>");
+            tracing::error!(%msg, "ContextGuard panicked during drop");
+        }
+    }
+}
+
+// region macro
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __guarded {
+    (
+        @parse action: [ $($action:tt)* ] ;
+        [ $($args:tt)* ] $body:block
+    ) => {
+        $crate::__guarded! {
+            action: [ $($action)* ]
+            mut: []
+            rest: [ $($args)* , ]
+            args: []
+            vars: []
+            body: [ $body ]
+        }
+    };
+
+    (
+        @parse action: [ $($action:tt)* ] ;
+        $body:block
+    ) => {
+        $crate::__guarded! {
+            @parse action: [ $($action)* ] ;
+            [] $body
+        }
+    };
+
+    (
+        @parse action: [ $($action:tt)* ] ;
+        [ $($args:tt)* ] $($body:tt)*
+    ) => {
+        $crate::__guarded! {
+            @parse action: [ $($action)* ] ;
+            [ $($args)* ] { $($body)* }
+        }
+    };
+
+    (
+        @parse action: [ $($action:tt)* ] ;
+        $($body:tt)*
+    ) => {
+        $crate::__guarded! {
+            @parse action: [ $($action)* ] ;
+            [] { $($body)* }
+        }
+    };
+
+    (
+        action: [ $($action:tt)* ]
+        mut: [ $($mut:tt)? ]
+        rest: [ mut $arg:ident , $($rest:tt)* ]
+        args: [ $($args:ident)* ]
+        vars: [ $($vars:tt)* ]
+        body: [ $body:expr ]
+    ) => {
+        $crate::__guarded! {
+            action: [ $($action)* ]
+            mut: [ mut ]
+            rest: [ $($rest)* ]
+            args: [ $($args)* $arg ]
+            vars: [ $($vars)* [mut $arg] ]
+            body: [ $body ]
+        }
+    };
+
+    (
+        action: [ $($action:tt)* ]
+        mut: [ $($mut:tt)? ]
+        rest: [ $arg:ident , $($rest:tt)* ]
+        args: [ $($args:ident)* ]
+        vars: [ $($vars:tt)* ]
+        body: [ $body:expr ]
+    ) => {
+        $crate::__guarded! {
+            action: [ $($action)* ]
+            mut: [ $($mut)? ]
+            rest: [ $($rest)* ]
+            args: [ $($args)* $arg ]
+            vars: [ $($vars)* [$arg] ]
+            body: [ $body ]
+        }
+    };
+
+    (
+        action: [ @stmt $guard:ident ]
+        mut: [ $($mut:tt)? ]
+        rest: [ $(,)* ]
+        args: [ $($args:ident)* ]
+        vars: [ $([$($vars:tt)*])* ]
+        body: [ $body:expr ]
+    ) => {
+        let $($mut)? $guard = $crate::utils::guard::ContextGuard::new(
+            ( $($args),* ),
+            |#[allow(unused_parens, unused_mut)] ( $($($vars)*),* )| $body
+        );
+
+        #[allow(unused_parens, unused_variables, clippy::toplevel_ref_arg)]
+        let ( $(ref $($vars)*),* ) = *$guard;
+    };
+
+    (
+        action: [ @expr ]
+        mut: [ $($mut:tt)? ]
+        rest: [ $(,)* ]
+        args: [ $($args:ident)* ]
+        vars: [ $([$($vars:tt)*])* ]
+        body: [ $body:expr ]
+    ) => {
+        $crate::utils::guard::ContextGuard::new(
+            ( $($args),* ),
+            |#[allow(unused_parens)] ( $($($vars)*),* )| $body
+        )
+    };
+}
+
+#[macro_export]
+macro_rules! guarded {
+    ( $guard:ident => $($tt:tt)* ) => {
+        $crate::__guarded! { @parse action: [ @stmt $guard ] ; $($tt)* }
+    };
+    ( $($tt:tt)* ) => {
+        $crate::__guarded! { @parse action: [ @stmt __guard ] ; $($tt)* }
+    };
+}
+
+#[macro_export]
+macro_rules! guard {
+    ( $($tt:tt)* ) => {
+        $crate::__guarded! { @parse action: [ @expr ] ; $($tt)* }
+    };
+}
+
+// endregion
+
+#[macro_export]
+macro_rules! defer {
+    ( $($tt:tt)* ) => {
+        $crate::guarded! { $($tt)* }
+    };
+}
--- easytier/src/utils/task.rs
+++ easytier/src/utils/task.rs
@@ -1,11 +1,15 @@
+use crate::utils::guard::ContextGuard;
 use std::future::Future;
 use std::io;
+use std::ops::DerefMut;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 use std::time::Duration;
 use tokio::task::JoinHandle;
 use tokio_util::sync::CancellationToken;
 use tokio_util::task::AbortOnDropHandle;
+
+// region CancellableTask
 
 #[derive(Debug)]
 pub struct CancellableTask<Output> {
@@ -72,5 +76,227 @@ impl<Output> Future for CancellableTask<Output> {
         Pin::new(&mut self.handle)
             .poll(cx)
             .map(|result| result.map_err(Into::into))
+    }
+}
+
+// endregion
+
+// region DetachableTask
+
+type BoxTask<Task> = Pin<Box<Task>>;
+
+struct DetachableTaskContext<Spawner, Task> {
+    spawner: Spawner,
+    task: Option<BoxTask<Task>>,
+}
+type DetachableTaskGuardHelper<Context> = ContextGuard<false, Context, fn(Context)>;
+type DetachableTaskGuard<Spawner, Task> =
+    DetachableTaskGuardHelper<DetachableTaskContext<Spawner, Task>>;
+
+pub struct DetachableTask<Spawner, Task> {
+    guard: DetachableTaskGuard<Spawner, Task>,
+}
+
+impl<Spawner, Task> DetachableTask<Spawner, Task> {
+    pub fn detach(self) {
+        self.guard.trigger()
+    }
+
+    pub fn reclaim(self) -> BoxTask<Task> {
+        self.guard.defuse().task.unwrap()
+    }
+}
+
+pub type TaskSpawner<Task, R = JoinHandle<<Task as Future>::Output>> = fn(BoxTask<Task>) -> R;
+
+impl DetachableTask<fn(()), ()> {
+    pub fn with_spawner<Spawner, _R, Task>(
+        spawner: Spawner,
+        task: Task,
+    ) -> DetachableTask<Spawner, Task>
+    where
+        Spawner: FnOnce(BoxTask<Task>) -> _R,
+    {
+        let context = DetachableTaskContext {
+            spawner,
+            task: Some(Box::pin(task)),
+        };
+        DetachableTask {
+            guard: crate::guard!([context] if let Some(task) = context.task {
+                (context.spawner)(task);
+            }),
+        }
+    }
+
+    pub fn new<Task>(task: Task) -> DetachableTask<TaskSpawner<Task>, Task>
+    where
+        Task: Future + Send + 'static,
+        <Task as Future>::Output: Send + 'static,
+    {
+        Self::with_spawner(|task| tokio::runtime::Handle::current().spawn(task), task)
+    }
+}
+
+impl<Spawner: FnOnce(BoxTask<Task>) -> _R, _R, Task> IntoFuture for DetachableTask<Spawner, Task>
+where
+    Task: Future,
+{
+    type Output = Task::Output;
+    type IntoFuture = DetachableTaskFuture<Spawner, Task>;
+
+    fn into_future(self) -> Self::IntoFuture {
+        DetachableTaskFuture { guard: self.guard }
+    }
+}
+
+pub struct DetachableTaskFuture<Spawner, Task> {
+    guard: DetachableTaskGuard<Spawner, Task>,
+}
+
+impl<Spawner: FnOnce(BoxTask<Task>) -> _R, _R, Task> Future for DetachableTaskFuture<Spawner, Task>
+where
+    Task: Future,
+{
+    type Output = Task::Output;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = unsafe { self.get_unchecked_mut() };
+        let context = this.guard.deref_mut();
+        let poll = context
+            .task
+            .as_mut()
+            .expect("polled after completion")
+            .as_mut()
+            .poll(cx);
+        if poll.is_ready() {
+            context.task.take();
+        }
+        poll
+    }
+}
+
+// endregion
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Arc;
+    use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+    use std::time::Duration;
+    use tokio::sync::{mpsc, oneshot};
+
+    #[tokio::test]
+    async fn spawn_when_dropped() {
+        let spawned = Arc::new(AtomicBool::new(false));
+        {
+            let spawned = spawned.clone();
+            let _task = DetachableTask::new(async move {
+                spawned.store(true, Ordering::SeqCst);
+            });
+        }
+
+        tokio::time::timeout(Duration::from_secs(1), async {
+            while !spawned.load(Ordering::SeqCst) {
+                tokio::task::yield_now().await;
+            }
+        })
+        .await
+        .expect("task should be spawned on drop");
+    }
+
+    #[tokio::test]
+    async fn await_completed_task_does_not_detach() {
+        let spawn_count = Arc::new(AtomicUsize::new(0));
+        let result = {
+            let spawn_count = spawn_count.clone();
+            DetachableTask::with_spawner(
+                move |_| {
+                    spawn_count.fetch_add(1, Ordering::SeqCst);
+                },
+                async { 7usize },
+            )
+            .await
+        };
+
+        assert_eq!(result, 7);
+        assert_eq!(spawn_count.load(Ordering::SeqCst), 0);
+    }
+
+    #[tokio::test]
+    async fn drop_without_await_and_runs_once() {
+        let spawn_count = Arc::new(AtomicUsize::new(0));
+        let (done_tx, done_rx) = oneshot::channel();
+
+        {
+            let spawn_count = spawn_count.clone();
+            let _task = DetachableTask::with_spawner(
+                move |f| {
+                    spawn_count.fetch_add(1, Ordering::SeqCst);
+                    tokio::spawn(async move {
+                        let result = f.await;
+                        let _ = done_tx.send(result);
+                    });
+                },
+                async { 42usize },
+            );
+        }
+
+        let detached_result = tokio::time::timeout(Duration::from_secs(1), done_rx)
+            .await
+            .expect("detached task should finish")
+            .expect("detached task should send result");
+
+        assert_eq!(detached_result, 42);
+        assert_eq!(spawn_count.load(Ordering::SeqCst), 1);
+    }
+
+    #[tokio::test]
+    async fn drop_after_await_still_detaches() {
+        let spawn_count = Arc::new(AtomicUsize::new(0));
+        let (value_tx, mut value_rx) = mpsc::channel(4);
+        let (done_tx, done_rx) = oneshot::channel();
+
+        let handle = {
+            let future = async move {
+                let mut sum = 0;
+                while let Some(value) = value_rx.recv().await {
+                    sum += value;
+                }
+                sum
+            };
+
+            let spawn_count = spawn_count.clone();
+            let task = DetachableTask::with_spawner(
+                move |f| {
+                    spawn_count.fetch_add(1, Ordering::SeqCst);
+                    tokio::spawn(async move {
+                        let result = f.await;
+                        let _ = done_tx.send(result);
+                    });
+                },
+                future,
+            );
+
+            tokio::spawn(task.into_future())
+        };
+
+        value_tx
+            .send(10)
+            .await
+            .expect("value receiver should still exist");
+        handle.abort();
+        value_tx
+            .send(11)
+            .await
+            .expect("value receiver should still exist");
+        drop(value_tx);
+
+        let detached_result = tokio::time::timeout(Duration::from_secs(1), done_rx)
+            .await
+            .expect("detached polled task should finish")
+            .expect("detached polled task should send result");
+
+        assert_eq!(detached_result, 21);
+        assert_eq!(spawn_count.load(Ordering::SeqCst), 1);
     }
 }
