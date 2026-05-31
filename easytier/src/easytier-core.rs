0ac9ef184ab02bc592506b2eb035d5b15ad39bcb
fanyang <fanyang89@outlook.com>
2026-05-31 12:38:37 +0800

    perf: tune jemalloc RSS decay

--- easytier/src/easytier-core.rs
+++ easytier/src/easytier-core.rs
@@ -14,12 +14,13 @@ static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 #[cfg(feature = "jemalloc-prof")]
 #[allow(non_upper_case_globals)]
 #[unsafe(export_name = "malloc_conf")]
-pub static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19,retain:false\0";
+pub static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19,dirty_decay_ms:1000,muzzy_decay_ms:1000,retain:false\0";
 
 #[cfg(not(feature = "jemalloc-prof"))]
 #[allow(non_upper_case_globals)]
 #[unsafe(export_name = "malloc_conf")]
-pub static malloc_conf: &[u8] = b"retain:false\0";
+pub static malloc_conf: &[u8] =
+    b"dirty_decay_ms:1000,muzzy_decay_ms:1000,retain:false\0";
 
 rust_i18n::i18n!("locales", fallback = "en");
 
