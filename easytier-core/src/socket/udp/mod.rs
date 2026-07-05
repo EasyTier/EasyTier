9e4ecbe09d9e0df65481cd183eb7891529356335
KKRainbow <443152178@qq.com>
2026-07-05 21:16:54 +0800

    core: split udp socket module
    Move the EasyTier UDP socket implementation into a socket/udp module tree with separate files for virtual socket bindings, packet parsing, session state, layer dispatch, and tests.
    
    Keep socket::udp public re-exports stable and leave UDP packet formats unchanged.

---
+++ easytier-core/src/socket/udp/mod.rs
@@ -0,0 +1,31 @@
+mod layer;
+mod packet;
+mod session;
+mod virtual_socket;
+
+#[cfg(test)]
+mod tests;
+
+const UDP_SESSION_RESEND_INTERVAL: std::time::Duration = std::time::Duration::from_millis(200);
+const UDP_SESSION_CONNECT_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(3);
+const UDP_SESSION_QUEUE_CAPACITY: usize = 128;
+
+pub use layer::{UdpSessionDialer, UdpSessionLayer};
+pub use packet::{
+    UdpSessionPacketError, extract_dst_addr_from_v4_hole_punch_packet,
+    extract_v6_hole_punch_packet, is_stun_packet, new_sack_packet, new_syn_packet,
+    new_v4_hole_punch_packet, new_v6_hole_punch_packet, parse_udp_session_datagram,
+};
+pub use session::{
+    UdpSession, UdpSessionConnectError, UdpSessionConnectRequest, UdpSessionConnector,
+    UdpSessionKind, UdpSessionLayerControl, UdpSessionListenRequest, UdpSessionListener,
+    UdpSessionProtocol, UdpSessionSocket,
+};
+pub(crate) use session::{
+    UdpSessionCleanup, UdpSessionCodec, UdpSessionOutbound, UdpSessionTunnelParts,
+};
+pub use virtual_socket::{
+    NoopUdpSessionControlHandler, PreferredIpv6Source, UdpBindOptions, UdpSessionControlHandler,
+    UdpSocketPurpose, VirtualUdpSocket, VirtualUdpSocketFactory, send_v4_hole_punch_control_packet,
+    send_v6_hole_punch_control_packet,
+};
---
+++ easytier-core/src/socket/udp/virtual_socket.rs
@@ -0,0 +1,200 @@
+use std::{
+    io,
+    net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6},
+    sync::Arc,
+};
+
+use async_trait::async_trait;
+
+use super::packet::{new_v4_hole_punch_packet, new_v6_hole_punch_packet};
+
+#[async_trait]
+pub trait VirtualUdpSocket: Send + Sync + 'static {
+    fn local_addr(&self) -> std::io::Result<SocketAddr>;
+
+    async fn send_to(&self, data: &[u8], addr: SocketAddr) -> std::io::Result<usize>;
+
+    async fn recv_from(&self, buf: &mut [u8]) -> std::io::Result<(usize, SocketAddr)>;
+}
+
+#[async_trait]
+pub trait UdpSessionControlHandler<S>: Send + Sync + 'static
+where
+    S: VirtualUdpSocket,
+{
+    async fn respond_stun(
+        &self,
+        _socket: Arc<S>,
+        _datagram: &[u8],
+        _remote_addr: SocketAddr,
+    ) -> io::Result<()> {
+        Ok(())
+    }
+
+    async fn send_v4_hole_punch(
+        &self,
+        _socket: Arc<S>,
+        _dst_addr: SocketAddrV4,
+    ) -> io::Result<usize> {
+        Ok(0)
+    }
+
+    async fn send_v6_hole_punch(
+        &self,
+        _socket: Arc<S>,
+        _dst_addr: SocketAddrV6,
+        _preferred_src: Option<PreferredIpv6Source>,
+    ) -> io::Result<usize> {
+        Ok(0)
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct NoopUdpSessionControlHandler;
+
+#[async_trait]
+impl<S> UdpSessionControlHandler<S> for NoopUdpSessionControlHandler where S: VirtualUdpSocket {}
+
+pub async fn send_v4_hole_punch_control_packet<F>(
+    factory: &F,
+    listener_port: u16,
+    dst_addr: SocketAddrV4,
+) -> anyhow::Result<()>
+where
+    F: VirtualUdpSocketFactory,
+{
+    let socket = factory
+        .bind_udp(
+            UdpBindOptions::hole_punch_control().with_local_addr(Some(SocketAddr::V4(
+                SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0),
+            ))),
+        )
+        .await?;
+    let packet = new_v4_hole_punch_packet(&dst_addr).into_bytes();
+    let listener_addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, listener_port));
+    socket.send_to(&packet, listener_addr).await?;
+    Ok(())
+}
+
+pub async fn send_v6_hole_punch_control_packet<F>(
+    factory: &F,
+    listener_port: u16,
+    dst_addr: SocketAddrV6,
+    preferred_src: Option<PreferredIpv6Source>,
+) -> anyhow::Result<()>
+where
+    F: VirtualUdpSocketFactory,
+{
+    let socket = factory
+        .bind_udp(
+            UdpBindOptions::hole_punch_control().with_local_addr(Some(SocketAddr::V6(
+                SocketAddrV6::new(Ipv6Addr::LOCALHOST, 0, 0, 0),
+            ))),
+        )
+        .await?;
+    let packet = new_v6_hole_punch_packet(&dst_addr, preferred_src).into_bytes();
+    let listener_addr = SocketAddr::V6(SocketAddrV6::new(Ipv6Addr::LOCALHOST, listener_port, 0, 0));
+    socket.send_to(&packet, listener_addr).await?;
+    Ok(())
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum UdpSocketPurpose {
+    HolePunchControl,
+    HolePunchCandidate,
+    DirectConnect,
+    PortBoundListener,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct UdpBindOptions {
+    pub local_addr: Option<SocketAddr>,
+    pub socket_mark: Option<u32>,
+    pub bind_device: Option<String>,
+    pub reuse_addr: bool,
+    pub reuse_port: bool,
+    pub only_v6: bool,
+    pub purpose: UdpSocketPurpose,
+}
+
+impl UdpBindOptions {
+    fn for_purpose(purpose: UdpSocketPurpose) -> Self {
+        Self {
+            local_addr: None,
+            socket_mark: None,
+            bind_device: None,
+            reuse_addr: false,
+            reuse_port: false,
+            only_v6: false,
+            purpose,
+        }
+    }
+
+    pub fn hole_punch_control() -> Self {
+        Self::for_purpose(UdpSocketPurpose::HolePunchControl)
+    }
+
+    pub fn hole_punch_candidate() -> Self {
+        Self::for_purpose(UdpSocketPurpose::HolePunchCandidate)
+    }
+
+    pub fn direct_connect() -> Self {
+        Self::for_purpose(UdpSocketPurpose::DirectConnect)
+    }
+
+    pub fn port_bound_listener(local_addr: SocketAddr) -> Self {
+        Self {
+            local_addr: Some(local_addr),
+            ..Self::for_purpose(UdpSocketPurpose::PortBoundListener)
+        }
+    }
+
+    pub fn with_local_addr(mut self, local_addr: Option<SocketAddr>) -> Self {
+        self.local_addr = local_addr;
+        self
+    }
+
+    pub fn with_socket_mark(mut self, socket_mark: Option<u32>) -> Self {
+        self.socket_mark = socket_mark;
+        self
+    }
+
+    pub fn with_bind_device(mut self, bind_device: Option<String>) -> Self {
+        self.bind_device = bind_device;
+        self
+    }
+
+    pub fn with_reuse_addr(mut self, reuse_addr: bool) -> Self {
+        self.reuse_addr = reuse_addr;
+        self
+    }
+
+    pub fn with_reuse_port(mut self, reuse_port: bool) -> Self {
+        self.reuse_port = reuse_port;
+        self
+    }
+
+    pub fn with_only_v6(mut self, only_v6: bool) -> Self {
+        self.only_v6 = only_v6;
+        self
+    }
+}
+
+impl Default for UdpBindOptions {
+    fn default() -> Self {
+        Self::hole_punch_control()
+    }
+}
+
+#[async_trait]
+pub trait VirtualUdpSocketFactory: Send + Sync + 'static {
+    type Socket: VirtualUdpSocket;
+
+    async fn bind_udp(&self, options: UdpBindOptions) -> anyhow::Result<Arc<Self::Socket>>;
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct PreferredIpv6Source {
+    pub ip: Ipv6Addr,
+    pub ifindex: u32,
+}
