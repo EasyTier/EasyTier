d9273de5ddebe216cd0a9eeee985a2da1c4b8892
KKRainbow <443152178@qq.com>
2026-07-11 18:52:31 +0800

    refactor: remove native DNS discovery logic

--- easytier/src/connector/dns_connector.rs
+++ easytier/src/connector/dns_connector.rs
@@ -1,37 +1,14 @@
-use std::{net::SocketAddr, sync::Arc};
+use std::net::SocketAddr;
 
 use super::{create_connector_by_url, http_connector::TunnelWithInfo};
 use crate::{
-    common::{
-        dns::{RESOLVER, resolve_txt_record},
-        error::Error,
-        global_ctx::ArcGlobalCtx,
-        log,
-    },
+    common::{dns::RuntimeDnsResolver, error::Error, global_ctx::ArcGlobalCtx},
     proto::common::TunnelInfo,
     tunnel::{IpScheme, IpVersion, Tunnel, TunnelConnector, TunnelError, TunnelScheme},
 };
 use anyhow::Context;
-use dashmap::DashSet;
-use hickory_resolver::proto::rr::rdata::SRV;
-use rand::{Rng as _, seq::SliceRandom};
+use easytier_core::{connectivity::manual::discovery, socket::SocketContext};
 use strum::VariantArray;
-
-fn weighted_choice<T>(options: &[(T, u64)]) -> Option<&T> {
-    let total_weight = options.iter().map(|(_, weight)| *weight).sum();
-    let mut rng = rand::thread_rng();
-    let rand_value = rng.gen_range(0..total_weight);
-    let mut accumulated_weight = 0;
-
-    for (item, weight) in options {
-        accumulated_weight += *weight;
-        if rand_value < accumulated_weight {
-            return Some(item);
-        }
-    }
-
-    None
-}
 
 #[derive(Debug)]
 pub struct DnsTunnelConnector {
@@ -55,27 +32,13 @@ impl DnsTunnelConnector {
 
     #[tracing::instrument(ret, err)]
     pub async fn resolve_txt_endpoint(&self, domain_name: &str) -> Result<url::Url, Error> {
-        let txt_data = resolve_txt_record(domain_name)
-            .await
-            .with_context(|| format!("resolve txt record failed, domain_name: {}", domain_name))?;
-
-        let candidate_urls = txt_data
-            .split(" ")
-            .map(|s| s.to_string())
-            .filter_map(|s| url::Url::parse(s.as_str()).ok())
-            .collect::<Vec<_>>();
-
-        // shuffle candidate_urls and get the first one
-        let url = candidate_urls
-            .choose(&mut rand::thread_rng())
-            .with_context(|| {
-                format!(
-                    "no valid url found, txt_data: {}, expecting an url list splitted by space",
-                    txt_data
-                )
-            })?;
-
-        Ok(url.clone())
+        discovery::resolve_txt_endpoint(
+            &RuntimeDnsResolver::new(),
+            domain_name,
+            SocketContext::default(),
+        )
+        .await
+        .map_err(Into::into)
     }
 
     pub async fn handle_txt_record(
@@ -86,77 +49,20 @@ impl DnsTunnelConnector {
         create_connector_by_url(url.as_str(), &self.global_ctx, self.ip_version).await
     }
 
-    fn handle_one_srv_record(record: &SRV, protocol: IpScheme) -> Result<(url::Url, u64), Error> {
-        // port must be non-zero
-        if record.port() == 0 {
-            return Err(anyhow::anyhow!("port must be non-zero").into());
-        }
-
-        let connector_dst = record.target().to_utf8();
-        let dst_url = format!("{}://{}:{}", protocol, connector_dst, record.port());
-
-        Ok((
-            dst_url.parse().with_context(|| {
-                format!(
-                    "parse dst_url failed, protocol: {}, connector_dst: {}, port: {}, dst_url: {}",
-                    protocol,
-                    connector_dst,
-                    record.port(),
-                    dst_url
-                )
-            })?,
-            record.priority() as _,
-        ))
-    }
-
     #[tracing::instrument(ret, err)]
     pub async fn resolve_srv_endpoint(&self, domain_name: &str) -> Result<url::Url, Error> {
-        tracing::info!("handle_srv_record: {}", domain_name);
-
-        let srv_domains = IpScheme::VARIANTS
+        let protocols = IpScheme::VARIANTS
             .iter()
-            .map(|s| (s, format!("_easytier._{}.{}", s, domain_name)))
+            .map(ToString::to_string)
             .collect::<Vec<_>>();
-        tracing::info!("build srv_domains: {:?}", srv_domains);
-        let responses = Arc::new(DashSet::new());
-        let srv_lookup_tasks = srv_domains
-            .iter()
-            .map(|(protocol, srv_domain)| {
-                let resolver = RESOLVER.clone();
-                let responses = responses.clone();
-                async move {
-                    let response = resolver.srv_lookup(srv_domain).await.with_context(|| {
-                        format!("srv_lookup failed, srv_domain: {}", srv_domain)
-                    })?;
-                    tracing::info!(?response, ?srv_domain, "srv_lookup response");
-                    for record in response.iter() {
-                        let parsed_record = Self::handle_one_srv_record(record, **protocol);
-                        tracing::info!(?parsed_record, ?srv_domain, "parsed_record");
-                        if let Err(e) = &parsed_record {
-                            log::warn!("got invalid srv record {:?}", e);
-                            continue;
-                        }
-                        responses.insert(parsed_record.unwrap());
-                    }
-                    Ok::<_, Error>(())
-                }
-            })
-            .collect::<Vec<_>>();
-        let _ = futures::future::join_all(srv_lookup_tasks).await;
-
-        let srv_records = responses.iter().map(|r| r.clone()).collect::<Vec<_>>();
-        if srv_records.is_empty() {
-            return Err(anyhow::anyhow!("no srv record found").into());
-        }
-
-        let url = weighted_choice(srv_records.as_slice()).with_context(|| {
-            format!(
-                "failed to choose a srv record, domain_name: {}, srv_records: {:?}",
-                domain_name, srv_records
-            )
-        })?;
-
-        Ok(url.clone())
+        discovery::resolve_srv_endpoint(
+            &RuntimeDnsResolver::new(),
+            domain_name,
+            &protocols,
+            SocketContext::default(),
+        )
+        .await
+        .map_err(Into::into)
     }
 
     pub async fn handle_srv_record(
